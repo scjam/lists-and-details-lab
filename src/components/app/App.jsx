69ad42fb8557aa68c057a5e7fe6468e5e7a00468
Sydney James <sydneyjames7@gmail.com>
2021-02-02 14:45:42 -0800

    file tree stuff

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
-import AllCharacters from '../character/AllCharacters';
+import AllCharacters from '../containers/character/AllCharacters.jsx';
 import Details from './containers/details/Details';
 
 export default class App extends React.Component {
