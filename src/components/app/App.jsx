b4328cf237b350c7422d73831dcf35cf33839501
Sydney James <sydneyjames7@gmail.com>
2021-02-02 14:38:10 -0800

    oops heres all the character code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from './containers/home/Home';
+import Details from './containers/details/Details';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main">
+          <Router>
+            <Switch>
+              <Route 
+                path="/"
+                exact
+                component = { Home }
+              />
+              <Route 
+                path="/details"
+                exact
+                component = { Details }
+              />
+            </Switch>
+          </Router>
+        </div>
+      </>
+    );
+  }
 }
